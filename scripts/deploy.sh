79c81006d8828a03425303fb5bb87750ebf5dcbd
Robin Joseph <robinmjoseph08@gmail.com>
2023-07-01 19:39:41 -0500

    chore(deploy): move to fly.io

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,21 +3,10 @@
 # Load any secrets.
 test -f .env && . .env
 
-CHART_VERSION=v0.1.0
 NOW=$(date +'%s')
 REPO="pokedextracker/pokedextracker.com"
 TAG="$(git rev-parse --short HEAD)"
 [[ -z $(git status -s) ]] || TAG="${TAG}-dirty-${NOW}"
-KUBE_CONTEXTS="$(kubectl config get-contexts -o name)"
-
-if ! echo "${KUBE_CONTEXTS}" | grep -q pokedextracker; then
-  echo
-  echo -e "\033[1;31m==> Couldn't find kube context with 'pokedextracker' in it. Make sure the cluster is configured correctly.\033[0m"
-  echo
-  exit 1
-fi
-
-KUBE_CONTEXT="$(echo "${KUBE_CONTEXTS}" | grep pokedextracker)"
 
 if ! docker buildx ls | grep -q multiarch; then
   echo
@@ -61,24 +50,13 @@ else
 fi
 
 echo
-echo -e "\033[1;32m==> Updating Helm repos\033[0m"
+echo -e "\033[1;32m==> Deploying ${TAG} to staging...\033[0m"
 echo
 
-helm repo update
-
-echo
-echo -e "\033[1;32m==> Deploying ${TAG} to staging on ${KUBE_CONTEXT}...\033[0m"
-echo
-
-helm upgrade \
-  --kube-context ${KUBE_CONTEXT} \
-  --install frontend \
-  --version ${CHART_VERSION} \
-  --namespace staging \
-  --values .kube/staging.yaml \
-  --set-string processes.web.image.tag=${TAG} \
-  --wait \
-  pokedextracker/app
+fly deploy \
+  --config .fly/staging.toml \
+  --image ${REPO}:${TAG} \
+  --vm-size shared-cpu-1x
 
 echo
 echo -e "\033[1;33m==> Deployed to staging. If everything looks good and you want to deploy to"
@@ -91,20 +69,15 @@ read proceed
 
 if [ "${proceed}" == "yes" ]; then
   echo
-  echo -e "\033[1;32m==> Deploying ${TAG} to production ${KUBE_CONTEXT}...\033[0m"
+  echo -e "\033[1;32m==> Deploying ${TAG} to production...\033[0m"
   echo
 
-  helm upgrade \
-    --kube-context ${KUBE_CONTEXT} \
-    --install frontend \
-    --version ${CHART_VERSION} \
-    --namespace production \
-    --values .kube/production.yaml \
-    --set-string processes.web.image.tag=${TAG} \
-    --wait \
-    pokedextracker/app
+  fly deploy \
+    --config .fly/production.toml \
+    --image ${REPO}:${TAG} \
+    --vm-size shared-cpu-1x
 else
   echo
-  echo -e "\033[1;33m==> Skipping deploying ${TAG} to production ${KUBE_CONTEXT}...\033[0m"
+  echo -e "\033[1;33m==> Skipping deploying ${TAG} to production...\033[0m"
   echo
 fi
